3f57ca4cbb7d01dfadf30b1f5bf8f9f3602d3edf
Jay Goodfellow <j.goodfellow@arcane.ws>
2018-10-18 12:43:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Parse Robots
+
+Promise based parser for robots.txt files. Allows webcrawlers and other scraping tools to easily check whether they have been blocked via the robots.txt file
+
+## Installation
+
+`npm install parse-robots --save`
+
+## Getting Started
+
+```js
+const Robots = require("parse-robots");
+
+const robot = await Robots("https://facebook.com/robots.txt");
+```
+
+## Methods
+
+- `isAllowed(path, agent)` - Boolean
+- `getSitemaps()` - Array
+- `getAgents()` - Array
+- `getCrawlDelay()` - Integer
+- `getHost()` - String
+
+## Examples
+
+```js
+const robot = await Robots("https://facebook.com/robots.txt");
+
+robot.isAllowed("/safetycheck/", `Mozilla/5.0 (compatible; SiteBot/1.0)`);      //false
+robot.isAllowed("/safetycheck/", `Mozilla/5.0 (compatible; Twitterbot/1.0)`);   //true
+robot.isAllowed("/live/", `Mozilla/5.0 (compatible; Twitterbot/1.0)`);          //false
+```
+
+## Errors
+
+If the robots file is unreachable (anything besides a 200) Robots will return `null`
+
+```js
+const robot = await Robots("https://example.com/robots.txt");
+
+if(!robot) return
+
+robot.isAllowed("/safetycheck/", `Mozilla/5.0 (compatible; SiteBot/1.0)`);      //false
+```
+
+## Maintainers
+
+- [Jay Goodfellow](https://github.com/jaygoodfellow)
+- [Arcane Digital Inc](https://github.com/arcanedigital)
+
+## License
+
+Copyright (c) 2017, Arcane & Jay Goodfellow.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,100 @@
+const fetch = require("./lib/fetch");
+module.exports = async function(url, options = {}) {
+  var _res = await fetch(url);
+  if (_res.status != 200) return null;
+  var _content = _res.content;
+  var _sitemaps = [];
+  var _agents = [];
+  var _groups = {};
+  var _crawldelay = null;
+  var _host = null;
+  _getGroups();
+  function _getGroups() {
+    var currentGroup = "";
+    var unsortedGroups = _res.content
+      .split(/\r?\n/)
+      .filter(row => row.match(/^(Allow|Disallow|User-agent).*/gim))
+      .reduce((acc, cur) => {
+        const key = cur
+          .split(":")[0]
+          .replace(/(\s?-\s)/g, "")
+          .toLowerCase()
+          .trim();
+        const value = cur
+          .split(":")[1]
+          .replace(/(\s?-\s)/g, "")
+          .trim();
+
+        if (key == "user-agent" && !acc.hasOwnProperty(value.toLowerCase()))
+          acc[value.toLowerCase()] = [];
+
+        if (key == "user-agent") currentGroup = value.toLowerCase();
+        if (key != "user-agent")
+          acc[currentGroup].push({ field: key, path: value });
+
+        return acc;
+      }, {});
+    _groups = unsortedGroups;
+    _agents = Object.keys(_groups).sort();
+  }
+
+  return {
+    isAllowed: function(path, agent) {
+      let allowed = true;
+
+      let activeGroup = _agents
+        .filter(_agent => {
+          if (agent.toLowerCase().indexOf(_agent) != -1) return true;
+        })
+        .reduce((acc, cur) => {
+          if (acc.length < cur.length) acc = cur;
+          return acc;
+        }, "*");
+
+      const rules = _groups[activeGroup].sort(function(a, b) {
+        return a.path.length - b.path.length;
+      });
+
+      for (let rule of rules) {
+        if (rule.path == "/") rule.path = "/*";
+        const pattern = rule.path
+          .replace(/\//gim, "\\/")
+          .replace(/\*/gim, ".+");
+        const reg = new RegExp(pattern, "ig");
+
+        if (path.match(reg) && rule.field == "disallow") allowed = false;
+        if (path.match(reg) && rule.field == "allow") allowed = true;
+      }
+
+      return allowed;
+    },
+    getGroups: function() {
+      return _groups;
+    },
+    getAgents: function() {
+      return _agents;
+    },
+    getSitemaps: function() {
+      const reg = /Sitemap: *([^\r\n]*)/gi;
+      var match = reg.exec(_content);
+
+      while (match != null) {
+        _sitemaps.push(match[1]);
+        match = reg.exec(_content);
+      }
+      return _sitemaps;
+    },
+    getCrawlDelay: function() {
+      const reg = /crawl-delay: *([^\r\n]*)/gi;
+      var match = reg.exec(_content);
+      if (match) _crawldelay = match[1];
+      return _crawldelay;
+    },
+    getHost: function() {
+      const reg = /host: *([^\r\n]*)/gi;
+      var match = reg.exec(_content);
+      if (match) _host = match[1];
+      return _host;
+    }
+  };
+};
